6942af94b75874ae58fa061b76e4e1167a7107b4
irfane98 <irfanemaman98@yahoo.fr>
2022-03-29 23:59:23 +0200

    add aws

---
+++ src/config/aws_exports.js
@@ -0,0 +1,8 @@
+export default {
+  aws_project_region: 'eu-west-3',
+  aws_cognito_region: 'eu-west-3',
+  aws_user_pools_id: 'eu-west-3_SMXU8m9JO',
+  aws_user_pools_web_client_id: '3kljbjqneq2bqh5ae9fv5moirs',
+  oauth: {}
+}
+// avec les miens ça ne marche pas donc j'ai essayé de mettre les votres voir.
